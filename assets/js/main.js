43a94aa040f3577f954785056b231c2157feea25
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-03-30 18:42:31 +0200

    modifiche estetiche + correzione js dadi- esercizio finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,92 +2,43 @@
 // Chiedi all’utente la sua email,
 // controlla che sia già presente nella lista di email per controllare se può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
-// Gioco dei dadi
+
+var listaMail = ["andrea.ferrini90@gmail.com", "newdante90@hotmail.it", "cielimmensi@gmail.com", "acquazzurracquachiara@gmail.com", "smellslikeateenspirit@gmail.com"];
+
+var checkMail = prompt('inserisci qui la tua email');
+var mail = false;
+
+for (var i = 0; i < listaMail.length; i++) {
+  var lista = listaMail[i];
+  if(lista == checkMail){
+    mail = true;
+  }
+}
+
+if (mail == true){
+  alert('Sei già registrato!');
+}else{
+  alert('Non sei già registrato!');
+}
+
+ // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+var nomeGiocatore1 = prompt("inserisci il nome del primo giocatore");
 
+var giocatore = document.getElementById('utente');
+giocatore.addEventListener('click', function(){
+  var punteggioUtente= Math.floor(Math.random() * 6) + 1;
+  alert(nomeGiocatore1+' hai realizzato '+punteggioUtente+" punti");
+  var punteggioCpu= Math.floor(Math.random() * 6) + 1;
+  alert('Il computer ha realizzato '+punteggioCpu+" punti");
 
-// let listaemail = ["andrea.ferrini90@gmail.com" , "newdante90@hotmail.it"]
-
-// let scriviemail = document.getElementById("email")
-
-// let verifica =  document.getElementById("verifica")
-
-// function controllaEmail(){
-
-//     scriviemail.value
-    
-//     scriviemail.push
- 
-//     let conferma = false
- 
- 
-//     for (let x = 0; x < listaemail.length; x++){
- 
-//      if(listaemail[x] === scriviemail.value){
- 
-//          conferma = true
-//          console.log("sei gia registrato")
-//      }
- 
-//     }
- 
-//     if(conferma == true){
- 
-//          verifica.innerText = `Sei già registrato!`
- 
-//     }else{
- 
-//          verifica.innerText = `Non sei già registrato!`
- 
-//     }
- 
-//  }
-
-
-//correzzione//
- function controlloEmail(){
-    let inputValue = document.getElementById(`inputEmail`).value
-
-    let arrayEmails = [
-        `andrea.ferrini90@gmail.com`,
-        `newdante90@hotmail.it` ,
-        `email1@gmail.com`,
-        `email2@gmail.com`,
-        `email3@gmail.com`,
-    ]
-
-    let soldatino = false;
-
-    for(let i = 0; i < arrayEmails.length; i++) {
-        if( arrayEmails[i] == inputValue ){
-            soldatino=true
-        }
-    }
-
-    if( soldatino == true ){
-        document.writeln("l' email che hai scelto è presente")
-    } else {
-        document.writeln("L' email che hai scelto NON è presente, REGISTRATI")
-    }
- }
-
- //dadi//
-
- function randomNumber (max, min) {
-    return Math.floor( Math.random() * max ) + min
- }
-
- function gioca(){
-    let cpu = randomNumber(6, 1)
-    let persona = randomNumber(6, 1)
-
-    if( cpu > persona ) {
-        document.writeln(`La Cpu ha vinto`)
-    } else if ( persona > cpu ){
-        document.writeln(`La persona ha vinto`)
-    } else {
-        document.writeln(`Pareggio`)
-    }
- }
+  if(punteggioUtente > punteggioCpu) {
+    alert('hai vinto!');
+  }else if (punteggioUtente == punteggioCpu) {
+    alert('il risultato è di parità, riprova');
+  }else {
+    alert('hai perso!');
+  }
+})
